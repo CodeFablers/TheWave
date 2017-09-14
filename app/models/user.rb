e4367a838a8ed473168e57446386ff5fc057a28c
Kris Aberfort <elhowm@gmail.com>
2017-09-14 21:34:53 +0300

    Fix mistake in library owner_id foreign key

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_one :own_library, class_name: 'Library', foreign_key: :onwer_id,
+  has_one :own_library, class_name: 'Library', foreign_key: :owner_id,
                         inverse_of: :owner
 end
